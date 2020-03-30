aa92a5b655824688b1984e5fcd5f270a860f0281
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-03-30 12:50:59 +0300

    add arrows to slider

--- src/doctor.php
+++ src/doctor.php
@@ -9,6 +9,28 @@ require "./modules/headlings.php";
 <script src="./assets/js/jquery-3.4.1.min.js"></script>
 </head>
 <body>
+<div class="layer1">
+    <div class="fig" id="fig11"></div>
+    <div class="fig" id="fig12"></div>
+    <div class="fig" id="fig13"></div>
+    <div class="fig" id="fig14"></div>
+</div>
+
+<div class="layer2">
+    <div class="fig" id="fig23"></div>
+    <div class="fig" id="fig24"></div>
+</div>
+
+<div class="layer3">
+    <div class="fig" id="fig31"></div>
+    <div class="fig" id="fig32"></div>
+    <div class="fig" id="fig33"></div>
+</div>
+
+<div class="layer4">
+    <div class="fig" id="fig21"></div>
+    <div class="fig" id="fig22"></div>
+</div>
 
 <!--Шапка-->
 <?php
