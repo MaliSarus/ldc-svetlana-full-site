5eb3194aa664e35ebf38b9fde16a5cce41d58f7a
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-04-07 18:35:44 +0300

    Create emergency-room.php and hurt.php

--- src/modules/hurts.php
+++ src/modules/hurts.php
@@ -4,12 +4,12 @@
             <div class="hurts__block">
                 <h2 class="title">С какими травмами к нам обращаются</h2>
                 <ul class="hurts__list">
-                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="#" class="hurts__list-link link_underline">Травмы глаза</a></li>
-                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="#" class="hurts__list-link link_underline">Травмы лица</a></li>
-                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="#" class="hurts__list-link link_underline">Травмы ЛОР-органов</a></li>
-                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="#" class="hurts__list-link link_underline">Травмы осевого скелета</a></li>
-                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="#" class="hurts__list-link link_underline">Рентген обследование</a></li>
-                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="#" class="hurts__list-link link_underline">Травмы кисти</a></li>
+                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="hurt.php" class="hurts__list-link link_underline">Травмы глаза</a></li>
+                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="hurt.php" class="hurts__list-link link_underline">Травмы лица</a></li>
+                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="hurt.php" class="hurts__list-link link_underline">Травмы ЛОР-органов</a></li>
+                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="hurt.php" class="hurts__list-link link_underline">Травмы осевого скелета</a></li>
+                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="hurt.php" class="hurts__list-link link_underline">Рентген обследование</a></li>
+                    <li class="hurts__list-item"><div class="list-item_red-square"></div><a href="hurt.php" class="hurts__list-link link_underline">Травмы кисти</a></li>
                 </ul>
             </div>
         </div>
