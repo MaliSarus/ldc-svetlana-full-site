cca104bdaf3a22459a713f130b59ef30fd6d89b4
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-04-08 19:00:19 +0300

    fixes check-ups.php

--- src/modules/checkup-features.php
+++ src/modules/checkup-features.php
@@ -6,23 +6,31 @@
                 <div class="checkup-features__slider">
                     <div class="checkup-features__item">
                         <div class="checkup-features__icon"><span class="checkup-features__icon-text">+</span></div>
-                        <div class="checkup-features__desc">Надежность </div>
-                        <div class="checkup-features__desc_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, magni.</div>
+                        <div class="checkup-features__desc">
+                            <span class="checkup-features__desc_text">Надежность</span>
+                            <span class="checkup-features__desc_text_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, sapiente?</span>
+                        </div>
                     </div>
                     <div class="checkup-features__item">
                         <div class="checkup-features__icon"><span class="checkup-features__icon-text">+</span></div>
-                        <div class="checkup-features__desc">Стабильность </div>
-                        <div class="checkup-features__desc_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, rem?</div>
+                        <div class="checkup-features__desc">
+                            <span class="checkup-features__desc_text">Стабильность</span>
+                            <span class="checkup-features__desc_text_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore</span>
+                        </div>
                     </div>
                     <div class="checkup-features__item">
                         <div class="checkup-features__icon"><span class="checkup-features__icon-text">+</span></div>
-                        <div class="checkup-features__desc">Преимущество </div>
-                        <div class="checkup-features__desc_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, perspiciatis.</div>
+                        <div class="checkup-features__desc">
+                            <span class="checkup-features__desc_text">Преимущество</span>
+                            <span class="checkup-features__desc_text_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, quas?</span>
+                        </div>
                     </div>
                     <div class="checkup-features__item">
                         <div class="checkup-features__icon"><span class="checkup-features__icon-text">+</span></div>
-                        <div class="checkup-features__desc">Качество </div>
-                        <div class="checkup-features__desc_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, laudantium?</div>
+                        <div class="checkup-features__desc">
+                            <span class="checkup-features__desc_text">Качество</span>
+                            <span class="checkup-features__desc_text_hover">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, necessitatibus.</span>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/modules/checkup.php
+++ src/modules/checkup.php
@@ -2,126 +2,126 @@
     <div class="container">
         <div class="row">
             <div class="checkup__block">
-                <div class="checkup__item">
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика заболеваний"</div>
                     <div class="checkup__item_info">
                         <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика щитовидной железы"</div>
                     <div class="checkup__item_info">
                         <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика заболеваний"</div>
                     <div class="checkup__item_info">
                         <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика щитовидной железы"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика заболеваний"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика щитовидной железы"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика заболеваний"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика щитовидной железы"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика заболеваний"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика щитовидной железы"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика заболеваний"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
-                <div class="checkup__item">
+                </a>
+                <a href="#" class="checkup__item">
                     <div class="checkup__item_title">Чек-Ап "Профилактика щитовидной железы"</div>
                     <div class="checkup__item_info">
                          <span class="checkup__item_price text_price">5 699 ₽</span>
                         <span class="text_price_del">15 699 ₽</span>
                         <div class="checkup__item_info_hover">
-                            <a href="check-up-simple.php" class="link">Узнать больше →</a>
+                            <div class="link">Узнать больше →</div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
     </div>
