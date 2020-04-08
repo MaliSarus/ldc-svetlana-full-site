92de5d452b9722e3d7d2dae77929a5b31eccb0b5
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-04-09 02:17:21 +0300

    fixes check-ups.php

--- src/check-up-simple.php
+++ src/check-up-simple.php
@@ -1,1 +1,62 @@
 <?php
+require "./modules/headlings.php";
+?>
+<title>ЛДЦ Светлана - Направление</title>
+<link rel="stylesheet" href="./assets/css/slick.css"/>
+<link rel="stylesheet" href="./assets/css/slick-theme.css"/>
+<link rel="stylesheet" href="./assets/css/magnific-popup.css">
+<script src="./assets/js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+
+<div class="layer1">
+    <div class="fig" id="fig11"></div>
+    <div class="fig" id="fig12"></div>
+    <div class="fig" id="fig13"></div>
+    <div class="fig" id="fig14"></div>
+</div>
+
+<div class="layer2">
+    <div class="fig" id="fig23"></div>
+    <div class="fig" id="fig24"></div>
+</div>
+
+<div class="layer3">
+    <div class="fig" id="fig31"></div>
+    <div class="fig" id="fig32"></div>
+    <div class="fig" id="fig33"></div>
+</div>
+
+<div class="layer4">
+    <div class="fig" id="fig21"></div>
+    <div class="fig" id="fig22"></div>
+</div>
+
+<?php
+require "./modules/popups.php";
+require "./modules/header.php";
+require "./modules/dropdown-menu.php";
+?>
+<!-------------------------------------------------------------------------------------------------------------------->
+<link rel="stylesheet" href="./assets/css/<?php echo basename(__FILE__, '.php');?>/style.css?ver=<?php echo  date("His")?>">
+
+
+<?php
+require "./modules/about-checkup.php";
+require "./modules/appointment.php";
+require "./modules/infoblock.php";
+require "./modules/checkup-features.php";
+require "./modules/specialists.php";
+require "./modules/feedback.php";
+?>
+
+
+
+<!----------------------------НЕ ТРОГАТЬ---------------------------------->
+<?
+require "./modules/footer.php";
+?>
+<!-------------------------------------------------------------------------------------------------------------------->
+
+
+
---
+++ src/modules/infoblock.php
@@ -0,0 +1,76 @@
+<section class="infoblock">
+    <div class="container">
+        <div class="row">
+            <div class="infoblock__block">
+                <div class="infoblock__item infoblock__item_with-image">
+                    <div class="infoblock__item_content">
+                        <h2 class="infoblock__item_title title">Заголовок блока с изображением</h2>
+                        <p class="infoblock__item_text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae, blanditiis deleniti
+                            dicta error hic, id ipsa ipsam itaque non quisquam quod voluptatibus. Aut consequatur deserunt
+                            hic magnam omnis quidem rem voluptas! Culpa enim et impedit incidunt minus neque officiis
+                            provident quibusdam ratione, repellendus. Aspernatur optio ratione veniam voluptate? Aperiam
+                            asperiores doloremque dolorum, expedita repellendus velit? Amet aperiam assumenda beatae dicta
+                            eveniet expedita fuga laudantium nihil perspiciatis temporibus? Animi aspernatur blanditiis
+                            consectetur corporis, dignissimos, fugiat iusto nemo obcaecati rem rerum, saepe soluta
+                            voluptates voluptatibus? Alias aliquid, beatae, consequuntur labore mollitia odio perspiciatis
+                            quaerat quisquam quos repellat repudiandae, tempora unde vel.
+                        </p>
+                    </div>
+                    <div class="infoblock__item_image" style="background-image: url(./assets/img/tools/1.jpg)"></div>
+                </div>
+                <div class="infoblock__item">
+                    <div class="infoblock__item_content">
+                        <h2 class="infoblock__item_title title">Заголовок блока без изображения</h2>
+                        <p class="infoblock__item_text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae, blanditiis deleniti
+                            dicta error hic, id ipsa ipsam itaque non quisquam quod voluptatibus. Aut consequatur deserunt
+                            hic magnam omnis quidem rem voluptas! Culpa enim et impedit incidunt minus neque officiis
+                            provident quibusdam ratione, repellendus. Aspernatur optio ratione veniam voluptate? Aperiam
+                            asperiores doloremque dolorum, expedita repellendus velit? Amet aperiam assumenda beatae dicta
+                            eveniet expedita fuga laudantium nihil perspiciatis temporibus? Animi aspernatur blanditiis
+                            consectetur corporis, dignissimos, fugiat iusto nemo obcaecati rem rerum, saepe soluta
+                            voluptates voluptatibus? Alias aliquid, beatae, consequuntur labore mollitia odio perspiciatis
+                            quaerat quisquam quos repellat repudiandae, tempora unde vel.
+                        </p>
+                    </div>
+                    <div class="infoblock__item_image"></div>
+                </div>
+                <div class="infoblock__item">
+                    <div class="infoblock__item_content">
+                        <h2 class="infoblock__item_title title">Заголовок блока без изображения</h2>
+                        <p class="infoblock__item_text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae, blanditiis deleniti
+                            dicta error hic, id ipsa ipsam itaque non quisquam quod voluptatibus. Aut consequatur deserunt
+                            hic magnam omnis quidem rem voluptas! Culpa enim et impedit incidunt minus neque officiis
+                            provident quibusdam ratione, repellendus. Aspernatur optio ratione veniam voluptate? Aperiam
+                            asperiores doloremque dolorum, expedita repellendus velit? Amet aperiam assumenda beatae dicta
+                            eveniet expedita fuga laudantium nihil perspiciatis temporibus? Animi aspernatur blanditiis
+                            consectetur corporis, dignissimos, fugiat iusto nemo obcaecati rem rerum, saepe soluta
+                            voluptates voluptatibus? Alias aliquid, beatae, consequuntur labore mollitia odio perspiciatis
+                            quaerat quisquam quos repellat repudiandae, tempora unde vel.
+                        </p>
+                    </div>
+                    <div class="infoblock__item_image"></div>
+                </div>
+                <div class="infoblock__item">
+                    <div class="infoblock__item_content">
+                        <h2 class="infoblock__item_title title">Заголовок блока без изображения</h2>
+                        <p class="infoblock__item_text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae, blanditiis deleniti
+                            dicta error hic, id ipsa ipsam itaque non quisquam quod voluptatibus. Aut consequatur deserunt
+                            hic magnam omnis quidem rem voluptas! Culpa enim et impedit incidunt minus neque officiis
+                            provident quibusdam ratione, repellendus. Aspernatur optio ratione veniam voluptate? Aperiam
+                            asperiores doloremque dolorum, expedita repellendus velit? Amet aperiam assumenda beatae dicta
+                            eveniet expedita fuga laudantium nihil perspiciatis temporibus? Animi aspernatur blanditiis
+                            consectetur corporis, dignissimos, fugiat iusto nemo obcaecati rem rerum, saepe soluta
+                            voluptates voluptatibus? Alias aliquid, beatae, consequuntur labore mollitia odio perspiciatis
+                            quaerat quisquam quos repellat repudiandae, tempora unde vel.
+                        </p>
+                    </div>
+                    <div class="infoblock__item_image"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
