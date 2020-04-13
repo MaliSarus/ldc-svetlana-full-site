c179699844ec741da55ae05101267cacec37e071
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-04-13 16:49:52 +0300

    fix check-up-simple.php

--- src/modules/infoblock.php
+++ src/modules/infoblock.php
@@ -19,8 +19,12 @@
                         </p>
                     </div>
                     <div class="infoblock__item_content">
-                        <h2 class="title">Заголовок блока</h2>
                         <img src="./assets/img/tools/1.jpg" alt="Картинка">
+                    </div>
+                </div>
+                <div class="infoblock__item ">
+                    <div class="infoblock__item_content">
+                        <h2 class="title">Заголовок блока</h2>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae, blanditiis deleniti
                             dicta error hic, id ipsa ipsam itaque non quisquam quod voluptatibus. Aut consequatur deserunt
@@ -34,11 +38,9 @@
                         </p>
                     </div>
                 </div>
-                <div class="infoblock__item">
+                <div class="infoblock__item ">
                     <div class="infoblock__item_content">
                         <h2 class="title">Заголовок блока</h2>
-                        <img src="./assets/img/tools/1.jpg" alt="Картинка">
-                        <img src="./assets/img/tools/1.jpg" alt="Картинка">
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae, blanditiis deleniti
                             dicta error hic, id ipsa ipsam itaque non quisquam quod voluptatibus. Aut consequatur deserunt
@@ -51,9 +53,21 @@
                             quaerat quisquam quos repellat repudiandae, tempora unde vel.
                         </p>
                     </div>
+                </div>
+                <div class="infoblock__item ">
                     <div class="infoblock__item_content">
-                        <img src="./assets/img/tools/1.jpg" alt="Картинка">
-                        <img src="./assets/img/tools/1.jpg" alt="Картинка">
+                        <h2 class="title">Заголовок блока</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae, blanditiis deleniti
+                            dicta error hic, id ipsa ipsam itaque non quisquam quod voluptatibus. Aut consequatur deserunt
+                            hic magnam omnis quidem rem voluptas! Culpa enim et impedit incidunt minus neque officiis
+                            provident quibusdam ratione, repellendus. Aspernatur optio ratione veniam voluptate? Aperiam
+                            asperiores doloremque dolorum, expedita repellendus velit? Amet aperiam assumenda beatae dicta
+                            eveniet expedita fuga laudantium nihil perspiciatis temporibus? Animi aspernatur blanditiis
+                            consectetur corporis, dignissimos, fugiat iusto nemo obcaecati rem rerum, saepe soluta
+                            voluptates voluptatibus? Alias aliquid, beatae, consequuntur labore mollitia odio perspiciatis
+                            quaerat quisquam quos repellat repudiandae, tempora unde vel.
+                        </p>
                     </div>
                 </div>
             </div>
