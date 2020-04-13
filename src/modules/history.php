eff89df33491423fadcdc0e1c6e8af527910404d
MaliSarus <43217693+MaliSarus@users.noreply.github.com>
2020-04-13 19:01:42 +0300

    fix doctors.php

--- src/modules/history.php
+++ src/modules/history.php
@@ -3,10 +3,10 @@
         <div class="row">
             <div class="history__block">
                 <div class="infoblock__item">
-                    <?php require "infoblock__content.php"?>
+                    
                 </div>
                 <div class="infoblock__item">
-                    <?php require "infoblock__content.php"?>
+
                 </div>
             </div>
         </div>
