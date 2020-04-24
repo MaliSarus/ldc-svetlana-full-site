71a190ff863ce262f8031bf30c9df72745197b69
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-04-24 17:40:25 +0300

    create all-articles.php

---
+++ src/all-articles.php
@@ -0,0 +1,130 @@
+<?php
+require "./modules/headlings.php";
+?>
+<title>ЛДЦ Светлана - Статьи</title>
+<link rel="stylesheet" href="./assets/css/slick.css"/>
+<link rel="stylesheet" href="./assets/css/slick-theme.css"/>
+<link rel="stylesheet" href="./assets/css/magnific-popup.css">
+<script src="./assets/js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+
+<div class="layer1">
+    <div class="fig" id="fig11"></div>
+    <div class="fig" id="fig12"></div>
+    <div class="fig" id="fig13"></div>
+    <div class="fig" id="fig14"></div>
+</div>
+
+<div class="layer2">
+    <div class="fig" id="fig23"></div>
+    <div class="fig" id="fig24"></div>
+</div>
+
+<div class="layer3">
+    <div class="fig" id="fig31"></div>
+    <div class="fig" id="fig32"></div>
+    <div class="fig" id="fig33"></div>
+</div>
+
+<div class="layer4">
+    <div class="fig" id="fig21"></div>
+    <div class="fig" id="fig22"></div>
+</div>
+
+<?php
+require "./modules/popups.php";
+require "./modules/header.php";
+require "./modules/dropdown-menu.php";
+?>
+<!-------------------------------------------------------------------------------------------------------------------->
+<link rel="stylesheet"
+      href="./assets/css/<?php echo basename(__FILE__, '.php'); ?>/style.css?ver=<?php echo date("His") ?>">
+
+<section class="all-articles">
+    <div class="container">
+        <div class="row">
+            <ul class="breadcrumbs">
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">О центре</a></li>
+            </ul>
+            <div class="all-articles__block">
+                <div class="all-articles__head">
+                    <h1 class="title">Статьи врачей лечебно-диагностического центра</h1>
+                </div>
+                <div class="science-articles__content">
+                    <div class="science-articles__content_item">
+                        <div class="science-articles__content_item_text" tabindex="0">
+                            Новые возможности в лазерном лечении варикозной
+                            новые возможностиварикоз ной новые возмо жностиварикозной новые возможности в лазерном...
+                        </div>
+                        <div class="science-articles__content_item_authors">
+                            <a class="science-articles__content_item_author link">Черноиван А.Г.</a>
+                            <a class="science-articles__content_item_author link">Вишневский А.А.</a>
+                        </div>
+                    </div>
+                    <div class="science-articles__content_item">
+                        <div class="science-articles__content_item_text" tabindex="0">
+                            Новые возможности в лазерном лечении варикозной новые возможностиварикоз ной новые возмо
+                            жностиварикозной новые возможности в лазерном...
+                            Новые возможности в лазерном лечении варикозной новые возможностиварикоз ной новые возмо
+                            жностиварикозной новые возможности в лазерном...
+                            Новые возможности в лазерном лечении варикозной новые возможностиварикоз ной новые возмо
+                            жностиварикозной новые возможности в лазерном...
+                        </div>
+                        <div class="science-articles__content_item_authors">
+                            <a class="science-articles__content_item_author link">Черноиван А.Г.</a>
+                            <a class="science-articles__content_item_author link">Вишневский А.А.</a>
+                        </div>
+                    </div>
+                    <div class="science-articles__content_item">
+                        <div class="science-articles__content_item_text" tabindex="0">
+                            Новые возможности в лазерном лечении варикозной
+                            новые возможностиварикоз ной новые возмо жностиварикозной новые возможности в лазерном...
+                        </div>
+                        <div class="science-articles__content_item_authors">
+                            <a class="science-articles__content_item_author link">Черноиван А.Г.</a>
+                            <a class="science-articles__content_item_author link">Вишневский А.А.</a>
+                        </div>
+                    </div>
+                    <div class="science-articles__content_item">
+                        <div class="science-articles__content_item_text" tabindex="0">
+                            Новые возможности в лазерном лечении варикозной новые возможностиварикоз ной новые возмо
+                            жностиварикозной новые возможности в лазерном...
+                            Новые возможности в лазерном лечении варикозной новые возможностиварикоз ной новые возмо
+                            жностиварикозной новые возможности в лазерном...
+                            Новые возможности в лазерном лечении варикозной новые возможностиварикоз ной новые возмо
+                            жностиварикозной новые возможности в лазерном...
+                        </div>
+                        <div class="science-articles__content_item_authors">
+                            <a class="science-articles__content_item_author link">Черноиван А.Г.</a>
+                            <a class="science-articles__content_item_author link">Вишневский А.А.</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="pagination">
+                    <a href="#" class="pagination-back"><img src="./assets/img/pagination/arrow.svg" alt="назад"></a>
+                    <ul class="pagination-list">
+                        <li class="pagination-list__item"><a href="#" class="pagination-list__link">1</a></li>
+                        <li class="pagination-list__item"><a href="#" class="pagination-list__link">2</a></li>
+                        <li class="pagination-list__item"><a href="#" class="pagination-list__link active">3</a></li>
+                        <li class="pagination-list__item"><a href="#" class="pagination-list__link">4</a></li>
+                        <li class="pagination-list__item"><a href="#" class="pagination-list__link">5</a></li>
+                        <li class="pagination-list__item"><a href="#" class="pagination-list__link">6</a></li>
+                    </ul>
+                    <a href="#" class="pagination-next"><img src="./assets/img/pagination/arrow.svg" alt="вперед"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!----------------------------НЕ ТРОГАТЬ---------------------------------->
+<?
+require "./modules/footer.php";
+?>
+<!-------------------------------------------------------------------------------------------------------------------->
+
+
+
